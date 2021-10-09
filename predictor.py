51d31c6d0091c4f113311a23d1fee2649b5ffab6
josecastrovazquez <joscasvaz@alum.us.es>
2021-10-09 19:32:38 +0200

    v 0.1
    Second beta version, main changes made on predictor script. Some folders added.

--- predictor.py
+++ predictor.py
@@ -1,6 +1,7 @@
 import csv
+from os import listdir
 
-def lee_fichero(fichero):
+def read_fixtures(fichero):
 
 	registros = []
 
@@ -12,7 +13,7 @@ def lee_fichero(fichero):
 
 	return registros
 
-def fixture(lectura):
+def mk_fixtures(lectura):
 
 	jornadas = []
 
@@ -54,101 +55,144 @@ def check_fixtures(fixtures):
 
 		i = i + 1
 
-def elo(team):
+def read_players(fichero):
 
-	elo = 0
+	registros = []
 
-	top = ('RMA','ATL','FCB','SEV')
-	midtop = ('VIL','RSO','BET','VAL','ATH')
-	midbot = ('CEL','OSA','CAD','GRA','LEV')
-	bottom = ('ESP','MLL','RAY','ALA','ELC','GET')
-	if team in top:
-		elo = 4
-	elif team in midtop:
-		elo = 3
-	elif team in midbot:
-		elo = 2
-	elif team in bottom:
-		elo = 1
+	with open(fichero, encoding='utf-8') as f:
+		lector = csv.reader(f)
+		next(lector)
+		for _,EQUIPO_ID,PUNTOS,PUNTOS_MEDIA,_,APODO,SLUG,_,POSICION,VALOR_MERCADO,ESTADO_JUGADOR,SEMANA_1,SEMANA_2,SEMANA_3,SEMANA_4,SEMANA_5,SEMANA_6,SEMANA_7,SEMANA_8,SEMANA_9,SEMANA_10,SEMANA_11,SEMANA_12,SEMANA_13,SEMANA_14,SEMANA_15,SEMANA_16,SEMANA_17,SEMANA_18,SEMANA_19,SEMANA_20,SEMANA_21,SEMANA_22,SEMANA_23,SEMANA_24,SEMANA_25,SEMANA_26,SEMANA_27,SEMANA_28,SEMANA_29,SEMANA_30,SEMANA_31,SEMANA_32,SEMANA_33,SEMANA_34,SEMANA_35,SEMANA_36,SEMANA_37,SEMANA_38,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_ in lector:
+			lista = [SLUG,APODO,EQUIPO_ID,POSICION,int(PUNTOS),float(PUNTOS_MEDIA),[int(VALOR_MERCADO)],[ESTADO_JUGADOR],[SEMANA_1,SEMANA_2,SEMANA_3,SEMANA_4,SEMANA_5,SEMANA_6,SEMANA_7,SEMANA_8,SEMANA_9,SEMANA_10,SEMANA_11,SEMANA_12,SEMANA_13,SEMANA_14,SEMANA_15,SEMANA_16,SEMANA_17,SEMANA_18,SEMANA_19,SEMANA_20,SEMANA_21,SEMANA_22,SEMANA_23,SEMANA_24,SEMANA_25,SEMANA_26,SEMANA_27,SEMANA_28,SEMANA_29,SEMANA_30,SEMANA_31,SEMANA_32,SEMANA_33,SEMANA_34,SEMANA_35,SEMANA_36,SEMANA_37,SEMANA_38]]
+			for i in lista[8]:
+				i = int(i)
+			registros.append(lista)
 
-	return elo
+	return registros
 
-def predict(fixtures):
+def read_performances(fichero):
 
-	i = 0
-	for fixture in fixtures:
+	registros = []
 
-		locales = []
-		visitantes = []
+	with open(fichero, encoding='utf-8') as f:
+		lector = csv.reader(f)
+		next(lector)
+		for _,SLUG,EQUIPO_ID,SEMANA,PUNTOS_TOTALES,MIN_JUGADOS,GOLES,ASISTENCIAS_GOL,ASISTENCIAS_SIN_GOL,LLEGADAS_AREA,PENALTIS_PROVOCADOS,PENALTIS_PARADOS,PARADAS,DESPEJES,PENALTIS_FALLADOS,GOLES_EN_PROPIA,GOLES_EN_CONTRA,TARJETAS_AMARILLAS,SEGUNDAS_AMARILLAS,TARJETAS_ROJAS,TIROS_A_PUERTA,REGATES,BALONES_RECUPERADOS,POSESIONES_PERDIDAS,PUNTOS_MARCA,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_ in lector:
+			lista = [SLUG,EQUIPO_ID,SEMANA,PUNTOS_TOTALES,MIN_JUGADOS,GOLES,ASISTENCIAS_GOL,ASISTENCIAS_SIN_GOL,LLEGADAS_AREA,PENALTIS_PROVOCADOS,PENALTIS_PARADOS,PARADAS,DESPEJES,PENALTIS_FALLADOS,GOLES_EN_PROPIA,GOLES_EN_CONTRA,TARJETAS_AMARILLAS,SEGUNDAS_AMARILLAS,TARJETAS_ROJAS,TIROS_A_PUERTA,REGATES,BALONES_RECUPERADOS,POSESIONES_PERDIDAS,PUNTOS_MARCA]
+			for i in lista[2:]:
+				lista[lista.index(i)] = int(i)
+			registros.append(lista)
 
-		j = 0
-		while j < len(fixture):
-			locales.append(fixtures[i][j][0])
-			visitantes.append(fixtures[i][j][1])
+	return registros
 
-			elo_local = 0
-			elo_visitante = 0
-			k = 0
-			while k < len(locales):
-				local = locales[k]
-				visitante = visitantes[k]
+def merge_players(fichero,ficheros_jugadores):
 
-				elo_local = elo(local) + 1
-				elo_visitante = elo(visitante)
-				fixture[j] = [[local,elo_local], [visitante,elo_visitante]]
+	registro = read_players(fichero)
+	slugs = []
 
-				veredict = -1
+	for jugador in registro:
+		slugs.append(jugador[0])
 
-				if elo_visitante < elo_local:
-					veredict = 1
-					if elo_visitante + 1 < elo_local:
-						veredict = 2
-				elif elo_visitante == elo_local:
-					veredict = 0
+	for archivo in ficheros_jugadores:
+		lectura = read_players(archivo)
+		for player in lectura:
+			slug = player[0]
+			if slug not in slugs:
+				registro.append(player)
+			else:
+				i = registro.index(registro[slugs.index(slug)])
+				registro[i][6].append(player[6][0])
+				registro[i][7].append(player[7][0])
 
-				k = k + 1
+	return registro
 
-			fixture[j].append(veredict)
-			j = j + 1
+def mk_players(jugadores, actuaciones):
+
+	players = []
+
+	for player in jugadores:
+		for actuacion in actuaciones:
 			
-		i = i + 1
+			if player[0] == actuacion[0]:
 
-	return fixtures
+				player_actuacion = actuacion[3:]
+				player_jornada = {actuacion[2]:player_actuacion}
+				player.append(player_jornada)
 
-def favoritos(fixtures):
+		players.append(player)
 
-	favoritos = {}
+	return players
 
-	for fixture in fixtures:
-		for match in fixture:
-			equipo_fav = ''
+def mk_teams(players):
 
-			if 1 < match[2]:
-				equipo_fav = match[0][0]
-			elif match[2] < 0:
-				equipo_fav = match[1][0]
-				match[2] = 1
+	teams = {}
 
-			if equipo_fav is not '':
-				if equipo_fav in favoritos.keys():
-					favoritos[equipo_fav] += match[2]
-				else:
-					favoritos[equipo_fav] = match[2]
+	for player in players:
 
-	return favoritos
+		equipo = player[2]
 
+		if equipo not in teams:
+			jugadores = [player]
+			teams[equipo] = jugadores
+		else:
+			teams[equipo].append(player)
 
-lectura = lee_fichero('./data/fixtures.csv')
-fixtures = fixture(lectura)
-check_fixtures(fixtures)
-predictions = predict(fixtures)
-favoritos = favoritos(fixtures)
+	return teams
 
-for prediction in predictions:
-	print('############')
-	for p in prediction:
-		print(p)
+def mk_elos(teams):
 
-print('############')
-for k,v in favoritos.items():
-	print(k, v)
+	equipos = {}
+
+	for k,v in teams.items():
+		team_elo = 0
+		for player in v:
+			team_elo = team_elo + player[4]
+		equipos[k] = [team_elo, v]
+
+	return equipos
+
+def mk_predictions(fixtures):
+	return 0
+
+def mk_strings(elo_teams):
+
+	registro = []
+
+	for k,v in elo_teams.items():
+		cadena = str(k)
+		for valor in v:
+			cadena = cadena + ',' + str(v[0]) + ','
+			for player in v[1]:
+				cadena = cadena + str(player)
+		cadena = cadena + '\n'
+		registro.append(cadena)
+
+	return registro
+
+def record(lista,fichero):
+
+	with open(fichero, mode='w', encoding='utf-8') as f:
+		escritor = csv.writer(f,quoting=csv.QUOTE_ALL)
+		escritor.writerow(lista)
+
+ruta2021 = './data/20-21/'
+ruta_archivos2021 = ['./data/20-21/'+ archivo for archivo in listdir(ruta2021) if archivo != 'players-performance.csv']
+ruta_archivos2021.sort()
+
+lectura_performances2021 = read_performances(ruta2021 + './players-performance.csv')
+lectura_jugadores2021 = [fichero for fichero in ruta_archivos2021]
+jugadores_merged = merge_players(lectura_jugadores2021[-1], lectura_jugadores2021[0:len(lectura_jugadores2021)-2])
+
+jugadores = mk_players(jugadores_merged,lectura_performances2021)
+equipos = mk_teams(jugadores)
+equipos_elo = mk_elos(equipos)
+cadenas = mk_strings(equipos_elo)
+
+print('¿Quieres escribir? Y/n')
+escribir = input()
+if(escribir == 'Y'):
+	escritura = record(cadenas,'./storage/20-21/players_st.csv')
+else:
+	print('No se escribió')
+
+print('Done.')
